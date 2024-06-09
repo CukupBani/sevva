044e0c2ecfb33a80d1de41507633565219e37c9f
CukupBani <paus.biru015@gmail.com>
2024-06-09 17:07:32 +0700

    little change

--- app/Product/page.tsx
+++ app/Product/page.tsx
@@ -21,7 +21,7 @@ const page = () => {
 
             <div className='grid grid-cols-4 gap-5'>
                 {KENDARAAN_DISEWAKAN.map((kendaraan) => (
-                    <ProductCard key={kendaraan.key} img={kendaraan.img} title={kendaraan.title} kategori={kendaraan.kategori} transmisi={kendaraan.transmisi} kapasitas={kendaraan.kapasitas} penyedia_jasa={kendaraan.penyedia_jasa} harga={kendaraan.harga} lokasi={kendaraan.lokasi} />
+                  <ProductCard key={kendaraan.key} img={kendaraan.img} title={kendaraan.title} kategori={kendaraan.kategori} transmisi={kendaraan.transmisi} kapasitas={kendaraan.kapasitas} penyedia_jasa={kendaraan.penyedia_jasa} harga={kendaraan.harga} lokasi={kendaraan.lokasi} />
                 ))}
             </div>
           </div>
